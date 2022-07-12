06d70b3ae72eddb28f4d9255510c40ab9b3e9d21
Wynne-Tran <wynnetran2018@gmail.com>
2022-07-12 03:32:20 -0400

    12.07.22 third

---
+++ leetcode/blind75/Arrays/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+package leetcode.blind75.Arrays;
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static void main(String []args){
+        int [] arr = {4, 9, 2, 4, 7, 6, 3, 3, 0, 5};
+       System.out.println(containsDuplicate(arr));
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> numbers = new HashSet<Integer>();
+        for (int num : nums) {
+            if (numbers.contains(num)) return true;
+            numbers.add(num);
+        }
+        return false;
+    }
+}
